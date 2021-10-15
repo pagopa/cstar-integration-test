e42c57ea8ac8c37a8226f5d6a95f3035c6d68f22
Giovanni <g.mancini.0@gmail.com>
2021-10-15 13:41:05 +0200

    [CEN-863] Add e2e uat test suite for payment manager

--- test/tests/bpdHbPaymentInstrumentsV1.js
+++ test/tests/bpdHbPaymentInstrumentsV1.js
@@ -2,12 +2,11 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export function GetBpdHbPaymentInstrumentSuccess(baseUrl, params, payInstrId) {
-      
+
   const res = http.get(
     `${baseUrl}/bpd/hb/payment-instruments/${payInstrId}`,
     params
   );
-  console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
 
   const isSuccessful = check(res, { 'Success': (r) => r.status === 200 });
   if (!isSuccessful) {
