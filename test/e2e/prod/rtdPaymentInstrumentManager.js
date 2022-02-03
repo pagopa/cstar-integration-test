d21d1a23829c09113d8dded885aad09671215a41
Giovanni <g.mancini.0@gmail.com>
2022-02-03 11:42:58 +0100

    [CEN-1340] Add test cases for backward compatibility

---
+++ test/e2e/prod/rtdPaymentInstrumentManager.js
@@ -0,0 +1,30 @@
+import { group } from 'k6';
+import {GetHashedPan } from '../../tests/rtdPaymentInstrumentManager.js';
+
+export let options = {}
+export let services = JSON.parse(open('../../../services/environments.json'));
+
+options.tlsAuth = [
+  { 
+    domains: [services.uat_issuer.baseUrl],
+    cert: open('../../../certs/cstar_p_test2.cert'),
+    key: open('../../../certs/cstar_u_test.key'),
+  }
+];
+
+export default () => {
+
+  group('Payment Instrument API', () => {
+    let params = {
+      headers: {
+        //'Ocp-Apim-Subscription-Key': `${__ENV.APIM_SK};product=issuer-api-product`,
+        //'Ocp-Apim-Subscription-Key': `${__ENV.APIM_SK};product=rtd-api-product`,
+        'Ocp-Apim-Subscription-Key': `${__ENV.APIM_SK}`,
+        'Ocp-Apim-Trace': 'true'
+      }
+    }
+
+    group('Should get hashed pans', () => GetHashedPan(services.prod_issuer.baseUrl, params) );
+
+  });
+}
--- test/tests/bpdIoCitizensV2.js
+++ test/tests/bpdIoCitizensV2.js
@@ -11,14 +11,36 @@ export function GetCitizenWithOptInStatusNOREQ(baseUrl, params, fiscalCode) {
     myParams
   ); 
 
-  console.log(res.json().optInStatus)
-
-
   const isSuccessful = check(res, {
     'Status: HTTP Success': (r) => r.status === 200,
     'Body: Fiscal Code is OK': (r) => r.json().fiscalCode === fiscalCode,
     'Body: Citizen is Enabled': (r) => r.json().enabled === true,
-    'Body: Citizen never received an OptIn request:': (r) => r.json().optInStatus === 'NOREQ'
+    'Body: Citizen never received an OptIn request': (r) => r.json().optInStatus === 'NOREQ'
+  });
+
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+  }
+
+}
+
+export function DeleteCitizen(baseUrl, params, fiscalCode) {
+  
+  let myParams = Object.assign({}, params)
+  myParams.headers.id = fiscalCode
+  myParams.headers['Content-Type'] = "application/json"
+
+  const res = http.del(
+    `${baseUrl}/bpd/io/citizen/v2`,
+    JSON.stringify({}),
+    myParams
+  );
+
+  const isSuccessful = check(res, {
+    'Status: HTTP Success': (r) => r.status === 204
+    // 'Body: Fiscal Code is OK': (r) => r.json().fiscalCode === fiscalCode,
+    // 'Body: Citizen is Enabled': (r) => r.json().enabled === false,
+    // 'Body: Citizen never received an OptIn request:': (r) => r.json().optInStatus === 'NOREQ'
   });
 
   if (!isSuccessful) {
@@ -31,6 +53,7 @@ export function PutCitizenWithOptInStatusACCEPTED(baseUrl, params, fiscalCode) {
   
   let myParams = Object.assign({}, params)
   myParams.headers.id = fiscalCode
+  myParams.headers['Content-Type'] = "application/json"
 
   const res = http.put(
     `${baseUrl}/bpd/io/citizen/v2`,
@@ -38,13 +61,110 @@ export function PutCitizenWithOptInStatusACCEPTED(baseUrl, params, fiscalCode) {
     myParams
   ); 
 
-  console.log(res.json().optInStatus)
+  const isSuccessful = check(res, {
+    'Status: HTTP Success': (r) => r.status === 200,
+    'Body: Fiscal Code is OK': (r) => r.json().fiscalCode === fiscalCode,
+    'Body: Citizen is Enabled': (r) => r.json().enabled === true,
+    'Body: Citizen ACCEPTED OptIn': (r) => r.json().optInStatus === 'ACCEPTED'
+  });
+
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+  }
+
+}
+
+export function PutCitizenWithOptInStatusDENIED(baseUrl, params, fiscalCode) {
+  
+  let myParams = Object.assign({}, params)
+  myParams.headers.id = fiscalCode
+  myParams.headers['Content-Type'] = "application/json"
+
+  const res = http.put(
+    `${baseUrl}/bpd/io/citizen/v2`,
+    JSON.stringify({optInStatus: 'DENIED'}),
+    myParams
+  ); 
 
   const isSuccessful = check(res, {
     'Status: HTTP Success': (r) => r.status === 200,
     'Body: Fiscal Code is OK': (r) => r.json().fiscalCode === fiscalCode,
     'Body: Citizen is Enabled': (r) => r.json().enabled === true,
-    'Body: Citizen ACCEPTED OptIn:': (r) => r.json().optInStatus === 'ACCEPTED'
+    'Body: Citizen DENIED OptIn': (r) => r.json().optInStatus === 'DENIED'
+  });
+
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+  }
+
+}
+
+export function PutCitizenWithOptInStatusNOREQAfterACCEPTED(baseUrl, params, fiscalCode) {
+  
+  let myParams = Object.assign({}, params)
+  myParams.headers.id = fiscalCode
+  myParams.headers['Content-Type'] = "application/json"
+
+  const res = http.put(
+    `${baseUrl}/bpd/io/citizen/v2`,
+    JSON.stringify({optInStatus: 'NOREQ'}),
+    myParams
+  ); 
+
+  const isSuccessful = check(res, {
+    'Status: HTTP Success': (r) => r.status === 200,
+    'Body: Fiscal Code is OK': (r) => r.json().fiscalCode === fiscalCode,
+    'Body: Citizen is Enabled': (r) => r.json().enabled === true,
+    'Body: Citizen ACCEPTED OptIn': (r) => r.json().optInStatus === 'ACCEPTED'
+  });
+
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+  }
+
+}
+
+export function PutCitizenWithOptInStatusNOREQAfterDENIED(baseUrl, params, fiscalCode) {
+  
+  let myParams = Object.assign({}, params)
+  myParams.headers.id = fiscalCode
+  myParams.headers['Content-Type'] = "application/json"
+
+  const res = http.put(
+    `${baseUrl}/bpd/io/citizen/v2`,
+    JSON.stringify({optInStatus: 'NOREQ'}),
+    myParams
+  ); 
+
+  const isSuccessful = check(res, {
+    'Status: HTTP Success': (r) => r.status === 200,
+    'Body: Fiscal Code is OK': (r) => r.json().fiscalCode === fiscalCode,
+    'Body: Citizen is Enabled': (r) => r.json().enabled === true,
+    'Body: Citizen DENIED OptIn': (r) => r.json().optInStatus === 'DENIED'
+  });
+
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+  }
+
+}
+
+export function PutCitizenWithoutOptInStatus(baseUrl, params, fiscalCode) {
+  
+  let myParams = Object.assign({}, params)
+  myParams.headers.id = fiscalCode
+  myParams.headers['Content-Type'] = "application/json"
+
+  const res = http.put(
+    `${baseUrl}/bpd/io/citizen/v2`,
+    JSON.stringify({}),
+    myParams
+  );
+
+  const isSuccessful = check(res, {
+    'Status: HTTP Success': (r) => r.status === 200,
+    'Body: Fiscal Code is OK': (r) => r.json().fiscalCode === fiscalCode,
+    'Body: Citizen is Enabled': (r) => r.json().enabled === true
   });
 
   if (!isSuccessful) {
