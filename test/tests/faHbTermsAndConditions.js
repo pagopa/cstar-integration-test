bd8468bb8dfb4258dd3e2b0f66682416a0b1defa
Giovanni <g.mancini.0@gmail.com>
2022-02-10 13:41:54 +0100

    [CEN-1350] Add fa T&C test suite and modify the bpd one

---
+++ test/tests/faHbTermsAndConditions.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export function GetFaTermsAndConditionsViaBlob(baseUrl, params) {
+
+  const res = http.get(
+    `${baseUrl}/pagopastorage/fa-terms-and-conditions/fa-tc.html`,
+    params
+  );
+
+  const isSuccessful = check(res, { 'Success': (r) => r.status === 200 });
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+  }
+}
+
+export function GetFaTermsAndConditions(baseUrl, params) {
+
+  const res = http.get(
+    `${baseUrl}/fa/tc/html`,
+    params
+  );
+
+  const isSuccessful = check(res, { 'Success': (r) => r.status === 200 });
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+  }
+}
