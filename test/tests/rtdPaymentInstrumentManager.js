1292ed7561d1df8c75078304edceb9cc980ab843
Alberto Re <alberto.re@pagopa.it>
2022-02-18 16:18:58 +0100

    [CEN-1377] Add test for Get Salt API

--- test/tests/rtdPaymentInstrumentManager.js
+++ test/tests/rtdPaymentInstrumentManager.js
@@ -1,23 +1,28 @@
-
 import http from 'k6/http';
 import { check } from 'k6';
 
 export function GetHashedPan(baseUrl, params ) {
-
-  // console.log(`${baseUrl}/rtd/payment-instrument-manager/hashed-pans`)
 
   const res = http.get(
     `${baseUrl}/rtd/payment-instrument-manager/hashed-pans`,
     params
   );
 
-  // const res = http.get(
-  //   'https://cstarpblobstorage.blob.core.windows.net/cstar-exports/hashedPans.zip',
-  //   params
-  // );
-  
   const isSuccessful = check(res, { 'Success': (r) => r.status === 200 });
   if (!isSuccessful) {
-    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`)
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`);
+  }
+}
+
+export function GetSalt(baseUrl, params ) {
+
+  const res = http.get(
+    `${baseUrl}/rtd/payment-instrument-manager/salt`,
+    params
+  );
+
+  const isSuccessful = check(res, { 'Success': (r) => r.status === 200 });
+  if (!isSuccessful) {
+    console.log(`Attempted ${res.headers['Ocp-Apim-Operationid']}. Unsuccessful. Response status ${res.status}. Please check trace ${res.headers['Ocp-Apim-Trace-Location']}`);
   }
 }
