ef139174432e995324523cb04c0039a487a175db
Alberto Re <alberto.re@pagopa.it>
2022-03-01 15:43:18 +0100

    [CEN-1418] Minor: defined API prefix as a variable

--- test/tests/rtdCsvTransactionDecrypted.js
+++ test/tests/rtdCsvTransactionDecrypted.js
@@ -1,10 +1,12 @@
 import http from 'k6/http';
 import { CheckStatusForbidden } from './common.js';
 
+const API_PREFIX = '/rtd/csv-transaction-decrypted';
+
 export function GetAdeSas(baseUrl, params ) {
 
   const res = http.post(
-    `${baseUrl}/rtd/csv-transaction-decrypted/ade/sas`,
+    `${baseUrl}${API_PREFIX}/ade/sas`,
 	{}, // Empty payload
     params
   )
@@ -15,7 +17,7 @@ export function GetAdeSas(baseUrl, params ) {
 export function GetRtdSas(baseUrl, params ) {
 
   const res = http.post(
-    `${baseUrl}/rtd/csv-transaction-decrypted/rtd/sas`,
+    `${baseUrl}${API_PREFIX}/rtd/sas`,
 	{}, // Empty payload
     params
   );
