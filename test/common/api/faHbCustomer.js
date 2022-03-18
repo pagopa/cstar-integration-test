f3f6f1682b1a727608aeb254781936fbad554319
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2022-03-18 12:13:02 +0100

    Add functions to call endpoints, add minimal load test (work in progress)

---
+++ test/common/api/faHbCustomer.js
@@ -0,0 +1,26 @@
+import http from 'k6/http'
+
+const API_PREFIX = '/fa/hb/customer'
+
+export function getFaCustomer(baseUrl, params, id, xRequestId) {
+    const myParams = Object.assign({}, params)
+    myParams.headers.id = id
+    if (xRequestId) {
+        myParams.headers['x-request-id'] = xRequestId
+    }
+    const res = http.get(`${baseUrl}${API_PREFIX}`, myParams)
+    __ENV.REQ_DUMP === undefined || console.log(JSON.stringify(res, null, 2))
+    return res
+}
+
+export function putFaCustomer(baseUrl, params, body) {
+    const myParams = Object.assign({}, params)
+    const res = http.put(
+        `${baseUrl}${API_PREFIX}`,
+        JSON.stringify(body),
+        myParams
+    )
+    console.log(JSON.stringify(res))
+    __ENV.REQ_DUMP === undefined || console.log(JSON.stringify(res, null, 2))
+    return res
+}
