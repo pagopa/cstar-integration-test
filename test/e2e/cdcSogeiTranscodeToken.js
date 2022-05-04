d78841620d03d19980ec461f859b1f7c67e7afcc
Giovanni <g.mancini.0@gmail.com>
2022-05-04 13:24:10 +0200

    [CDC-58] refactor SOGEI Api Test to allow token transcode (#32)

---
+++ test/e2e/cdcSogeiTranscodeToken.js
@@ -0,0 +1,43 @@
+import { group } from 'k6'
+import exec from 'k6/execution'
+import { sogeiHealthCheck } from '../common/api/cdcSogeiHealthCheck.js'
+import { loginFullUrl } from '../common/api/bpdIoLogin.js'
+import { assert, statusOk } from '../common/assertions.js'
+import { isEnvValid, isTestEnabledOnEnv, UAT } from '../common/envs.js'
+import dotenv from 'k6/x/dotenv'
+
+const REGISTERED_ENVS = [UAT]
+
+const services = JSON.parse(open('../../services/environments.json'))
+let baseUrl
+let myEnv
+
+if (isEnvValid(__ENV.TARGET_ENV)) {
+    myEnv = dotenv.parse(open(`../../.env.${__ENV.TARGET_ENV}.local`))
+    baseUrl = services[`${__ENV.TARGET_ENV}_io`].baseUrl
+}
+
+export function setup() {
+    const authToken = loginFullUrl(`${baseUrl}/bpd/pagopa/api/v1/login`, myEnv.FISCAL_CODE_EXISTING)
+    return {
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+            'Ocp-Apim-Subscription-Key': `${myEnv.APIM_SK};product=app-io-product`,
+            'Ocp-Apim-Trace': 'true',
+        },
+    }
+}
+
+export default (params) => {
+    if (
+        !isEnvValid(__ENV.TARGET_ENV) ||
+        !isTestEnabledOnEnv(__ENV.TARGET_ENV, REGISTERED_ENVS)
+    ) {
+        return
+    }
+    group('SOGEI CdC', () => {
+        group('Should GET HealthCheck', () =>
+            assert(sogeiHealthCheck(baseUrl, params), [statusOk()])
+        )
+    })
+}
