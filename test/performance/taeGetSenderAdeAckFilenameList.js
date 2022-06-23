2d3d4eacf4c1a16f81130d5fd8f80b5cb374a57a
Andrea Morabito <andrea.morabito@pagopa.it>
2022-06-23 17:54:50 +0200

    [RTD-198] Add load tests for TAE

---
+++ test/performance/taeGetSenderAdeAckFilenameList.js
@@ -0,0 +1,61 @@
+import { group } from 'k6'
+import exec from 'k6/execution'
+import { assert, statusOk } from '../common/assertions.js'
+import {
+    isEnvValid,
+    isTestEnabledOnEnv,
+    DEV,
+    UAT,
+    PROD,
+} from '../common/envs.js'
+import dotenv from 'k6/x/dotenv'
+import { getSenderAdeAckFileNameList } from '../common/api/rtdFileRegister.js'
+
+const REGISTERED_ENVS = [DEV, UAT, PROD]
+
+const services = JSON.parse(open('../../services/environments.json'))
+export let options = {
+    stages: [
+        { duration: '1m', target: 10 },
+        { duration: '3m', target: 30 },
+        { duration: '1m', target: 10 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<500'],
+    },
+}
+let params = {}
+let baseUrl
+let myEnv
+
+if (isEnvValid(__ENV.TARGET_ENV)) {
+    myEnv = dotenv.parse(open(`../../.env.${__ENV.TARGET_ENV}.local`))
+    baseUrl = services[`${__ENV.TARGET_ENV}_issuer`].baseUrl
+
+    options.tlsAuth = [
+        {
+            domains: [baseUrl],
+            cert: open(`../../certs/${myEnv.MAUTH_CERT_NAME}`),
+            key: open(`../../certs/${myEnv.MAUTH_PRIVATE_KEY_NAME}`),
+        },
+    ]
+
+    params.headers = {
+        'Ocp-Apim-Subscription-Key': myEnv.APIM_RTDPRODUCT_SK,
+    }
+}
+
+// In performance tests we shall use abort() to prevent the execution
+// of the default function, otherwise the VUs will be spawned
+if (!isTestEnabledOnEnv(__ENV.TARGET_ENV, REGISTERED_ENVS)) {
+    console.log('Test not enabled for target env')
+    exec.test.abort()
+}
+
+export default () => {
+    group('TAE SenderAdeAck API', () => {
+        group('Should retrieve filename list', () => {
+            assert(getSenderAdeAckFileNameList(baseUrl, params), [statusOk()])
+        })
+    })
+}
