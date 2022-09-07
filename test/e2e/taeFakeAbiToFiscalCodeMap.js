ea31e51968db6cadc9312f1e02efb874ff3acb21
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2022-09-07 15:43:24 +0200

    [RTD-446] Add test e2e for abi to fiscal code map api (#42)
    * [RTD-446] Add test e2e for abi to fiscal code map api
    
    * [RTD-446] Add assertions on abi-fc map conversion

---
+++ test/e2e/taeFakeAbiToFiscalCodeMap.js
@@ -0,0 +1,44 @@
+import { group } from 'k6'
+import { assert, statusOk, bodyJsonSelectorValue } from '../common/assertions.js'
+import { isEnvValid, isTestEnabledOnEnv, DEV } from '../common/envs.js'
+import dotenv from 'k6/x/dotenv'
+import { randomFiscalCode } from '../common/utils.js'
+import { getAbiToFiscalCodesMap } from '../common/api/taeAbiToFiscalCodes.js'
+
+
+const REGISTERED_ENVS = [DEV]
+
+const services = JSON.parse(open('../../services/environments.json'))
+let params = {}
+let options = {}
+let baseUrl
+let myEnv
+
+if (isEnvValid(__ENV.TARGET_ENV)) {
+    myEnv = dotenv.parse(open(`../../.env.${__ENV.TARGET_ENV}.local`))
+    baseUrl = services[`${__ENV.TARGET_ENV}_io`].baseUrl
+
+    options.tlsAuth = [
+        {
+            domains: [baseUrl],
+            cert: open(`../../certs/${myEnv.MAUTH_CERT_NAME}`),
+            key: open(`../../certs/${myEnv.MAUTH_PRIVATE_KEY_NAME}`),
+        },
+    ]
+
+    params.headers = {
+        'Ocp-Apim-Subscription-Key': myEnv.APIM_RTDPRODUCT_SK,
+    }
+}
+
+export default () => {
+    group('TAE AbiToFiscalCode API', () => {
+        group('Should retrieve conversion map', () =>
+            assert(getAbiToFiscalCodesMap(baseUrl, params), [statusOk(),
+            bodyJsonSelectorValue('STPAY', 'LU30726739'),
+            bodyJsonSelectorValue('BPAY1', '04949971008'),
+            bodyJsonSelectorValue('SUMUP', 'IE9813461A'),
+            bodyJsonSelectorValue('ICARD', 'BG175325806')])
+        )
+    })
+}
