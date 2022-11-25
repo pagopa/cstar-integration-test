10c553e3e387d3b3a617a27b0e8cb88d1a309e9e
Giovanni <g.mancini.0@gmail.com>
2022-11-25 16:31:22 +0100

    [ISB-487] Add PM Wallet test

---
+++ test/e2e/pmTestIdWallet.js
@@ -0,0 +1,49 @@
+import { group } from 'k6'
+import { getPaymentInstrumentList } from '../common/api/pmWalletExt.js'
+import { assert, statusOk,  } from '../common/assertions.js'
+import { isEnvValid, isTestEnabledOnEnv, DEV, UAT, PROD } from '../common/envs.js'
+import dotenv from 'k6/x/dotenv'
+
+const REGISTERED_ENVS = [DEV, UAT, PROD]
+
+const services = JSON.parse(open('../../services/environments.json'))
+
+export const options = {}
+const params = {}
+let baseUrl
+let myEnv
+
+if (isEnvValid(__ENV.TARGET_ENV)) {
+    myEnv = dotenv.parse(open(`../../.env.${__ENV.TARGET_ENV}.local`))
+    baseUrl = services[`${__ENV.TARGET_ENV}_issuer`].baseUrl
+
+    options.tlsAuth = [
+        {
+            domains: [baseUrl],
+            cert: open(`../../certs/${myEnv.MAUTH_CERT_NAME}`),
+            key: open(`../../certs/${myEnv.MAUTH_PRIVATE_KEY_NAME}`),
+        },
+    ]
+
+    params.headers = {
+        'Ocp-Apim-Subscription-Key': myEnv.APIM_PM_PRODUCT_SK + ';product=pm-api-product',
+        'Ocp-Apim-Trace': 'true',
+        'Content-Type': 'application/json',
+    }
+}
+
+export default () => {
+    if (
+        !isEnvValid(__ENV.TARGET_ENV) ||
+        !isTestEnabledOnEnv(__ENV.TARGET_ENV, REGISTERED_ENVS)
+    ) {
+        return
+    }
+    group("PM wallet-ext API", () => {
+        group("Should get a list of user's wallets", () => {
+            assert(getPaymentInstrumentList(baseUrl, params, myEnv.FISCAL_CODE_EXISTING), [
+                statusOk(),
+            ])
+        })
+    })
+}
