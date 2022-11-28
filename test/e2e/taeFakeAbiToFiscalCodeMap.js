379f52d16b39667dfbaf73e286ee4ce5322e98de
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2022-11-28 12:14:00 +0100

    run pre-commit

--- test/e2e/taeFakeAbiToFiscalCodeMap.js
+++ test/e2e/taeFakeAbiToFiscalCodeMap.js
@@ -1,10 +1,13 @@
 import { group } from 'k6'
-import { assert, statusOk, bodyJsonSelectorValue } from '../common/assertions.js'
+import {
+    assert,
+    statusOk,
+    bodyJsonSelectorValue,
+} from '../common/assertions.js'
 import { isEnvValid, isTestEnabledOnEnv, DEV } from '../common/envs.js'
 import dotenv from 'k6/x/dotenv'
 import { randomFiscalCode } from '../common/utils.js'
 import { getAbiToFiscalCodesMap } from '../common/api/taeAbiToFiscalCodes.js'
-
 
 const REGISTERED_ENVS = [DEV]
 
@@ -34,11 +37,13 @@ if (isEnvValid(__ENV.TARGET_ENV)) {
 export default () => {
     group('TAE AbiToFiscalCode API', () => {
         group('Should retrieve conversion map', () =>
-            assert(getAbiToFiscalCodesMap(baseUrl, params), [statusOk(),
-            bodyJsonSelectorValue('STPAY', 'LU30726739'),
-            bodyJsonSelectorValue('BPAY1', '04949971008'),
-            bodyJsonSelectorValue('SUMUP', 'IE9813461A'),
-            bodyJsonSelectorValue('ICARD', 'BG175325806')])
+            assert(getAbiToFiscalCodesMap(baseUrl, params), [
+                statusOk(),
+                bodyJsonSelectorValue('STPAY', 'LU30726739'),
+                bodyJsonSelectorValue('BPAY1', '04949971008'),
+                bodyJsonSelectorValue('SUMUP', 'IE9813461A'),
+                bodyJsonSelectorValue('ICARD', 'BG175325806'),
+            ])
         )
     })
 }
