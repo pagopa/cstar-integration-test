bb71540d5c278ea4aad26f38def9c427c59138ae
petretiandrea <andrea.petreti@pagopa.it>
2022-12-02 11:11:13 +0100

    [RTD-1050] Acceptance Test tkm rtd apim integration (#53)
    * added token manager api get pub key test
    
    * add token manager upload token file smoke tests
    
    * add test to get known hashpans
    
    * add get bin range smoke test

---
+++ test/common/api/rtdTokenManagerApi.js
@@ -0,0 +1,50 @@
+import http from 'k6/http'
+
+const API_PREFIX = "/rtd/token";
+const PUBLIC_KEY_PATH = "/token-list/public-key";
+const UPLOAD_TOKEN_FILE_PATH = "/token-list";
+const KNOWN_HASHES_PATH = "/known-hashes/links";
+const BIN_RANGES_PATH = "/bin-ranges/links";
+
+export default class RtdTokenManagerApi {
+
+    constructor(baseUrl, subscriptionKey) {
+        this.baseUrl = baseUrl;
+        this.headers = {
+            'Ocp-Apim-Subscription-Key': subscriptionKey,
+            'Ocp-Apim-Trace': 'true'
+        }
+    }
+
+    getPublicKey() {
+        return http.get(this.createUrl(PUBLIC_KEY_PATH), {
+            headers: this.headers
+        });
+    }
+
+    uploadTokenFile(binaryFile, fileName) {
+        const multipart = {
+            file: http.file(binaryFile, fileName)
+        };
+        return http.post(this.createUrl(UPLOAD_TOKEN_FILE_PATH), multipart, {
+            headers: this.headers
+        });
+    }
+
+    getKnownHashes() {
+        return http.get(this.createUrl(KNOWN_HASHES_PATH), {
+            headers: this.headers
+        })
+    }
+
+    getBinRanges() {
+        return http.get(this.createUrl(BIN_RANGES_PATH), {
+            headers: this.headers
+        });
+    }
+
+    createUrl(path) {
+        return `${this.baseUrl}${API_PREFIX}${path}`;
+    }
+
+}
---
+++ test/smoke/rtdTokenManagerIntegration.js
@@ -0,0 +1,49 @@
+import { group, check } from 'k6'
+import {assert, bodyJsonReduceArray, bodyPgpPublicKey, statusOk} from '../common/assertions.js'
+import RtdTokenManagerApi from "../common/api/rtdTokenManagerApi.js";
+import {setupEnvironment} from "./setupenv.js";
+
+const {env, baseUrl} = setupEnvironment('../../services/environments.json');
+const stubTokenFile = open("stub-files/tokenFile.csv");
+export const options = {
+    tlsAuth: [
+        {
+            domains: [baseUrl],
+            cert: open(`../../certs/${env.MAUTH_CERT_NAME}`),
+            key: open(`../../certs/${env.MAUTH_PRIVATE_KEY_NAME}`),
+        },
+    ]
+}
+
+export default () => {
+    group('RTD Token Manager Integration API', () => {
+        const api = new RtdTokenManagerApi(baseUrl, env.APIM_RTDPRODUCT_SK);
+        group('Should get public key', () =>
+            assert(api.getPublicKey(), [
+                statusOk(),
+                bodyPgpPublicKey()
+            ])
+        )
+
+        group('Should upload a token file', () =>
+            assert(api.uploadTokenFile(stubTokenFile, 'tokenFile.csv'), [
+                statusOk(),
+                (res) => check(res, {
+                    "response include filename": res.json()["filename"].includes('tokenFile')
+                })
+            ])
+        )
+
+        group("Should get known hashes", () =>
+            assert(api.getKnownHashes(), [
+                statusOk()
+            ])
+        )
+
+        group("Should get bin ranges", () =>
+            assert(api.getBinRanges(), [
+                statusOk()
+            ])
+        )
+    })
+}
