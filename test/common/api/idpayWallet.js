9347a6e47d53faeeb03d45e8842606485e1d19f4
larissaaiala <larissaaialasilva@gmail.com>
2023-01-11 10:51:05 +0100

    [IDP-65] Test di enrollment massivo

--- test/common/api/idpayWallet.js
+++ test/common/api/idpayWallet.js
@@ -2,12 +2,11 @@ import http from 'k6/http'
 
 const API_PREFIX = '/idpay'
 
-export function putEnrollInstrumentIssuer(baseUrl, body, params, initiativeId) {
-    const myParams = Object.assign({}, params)
+export function putEnrollInstrumentIssuer(baseUrl, body, headers, initiativeId) {
     const res = http.put(
-        `${baseUrl}${API_PREFIX}hb/wallet/${initiativeId}/instruments`,
+        `${baseUrl}${API_PREFIX}/hb/wallet/${initiativeId}/instruments`,
         body,
-        myParams
+        headers
     )  
     __ENV.REQ_DUMP === undefined || console.log(JSON.stringify(res, null, 2))
     return res
