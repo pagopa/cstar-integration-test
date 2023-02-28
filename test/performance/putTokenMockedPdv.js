046ef58566005bd45e6fced33e01a3a717e1893c
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2023-02-28 16:29:07 +0100

    IDP-442 added log for test details

--- test/performance/putTokenMockedPdv.js
+++ test/performance/putTokenMockedPdv.js
@@ -26,7 +26,7 @@ export let options = {
 //            timeUnit: '1s', //period of time to apply the iteration
 //            startRate: 10, //Number of iterations to execute each timeUnit period at test start.
 //            preAllocatedVUs: 500,
-//            stages: [
+//            stages: [ //TODO to be parametrized according to the number of VUs
 //                { duration: '1s', target: 10 },
 //                { duration: '1s', target: 50 },
 //                { duration: '1s', target: 10 },
@@ -40,16 +40,16 @@ export let options = {
      scenarios: { //TODO only test scope
         scenario_uno: {
             executor: 'per-vu-iterations',
-            vus: 10,
+            vus: __ENV.VIRTUAL_USERS_ENV,
             iterations: 1,
             startTime: '0s',
-            maxDuration: '600s',
+            maxDuration: '600s', //TODO to be parametrized according to the number of VUs
         },
     },
     thresholds: {
         http_req_failed: [{threshold:'rate<0.01', abortOnFail: false, delayAbortEval: '10s'},], // http errors should be less than 1%
 //        http_req_duration: [{threshold:'p(90)<500', abortOnFail: false, delayAbortEval: '10s'},], // 90% of requests should be below 200ms
-        http_reqs: [{threshold:'rate<500', abortOnFail: false, delayAbortEval: '10s'},] //
+        http_reqs: [{threshold: `count<=${__ENV.VIRTUAL_USERS_ENV}`, abortOnFail: false, delayAbortEval: '10s'},]
       },
     
 }
@@ -95,6 +95,14 @@ export default () => {
 }
 
 export function handleSummary(data){
+    console.log(`
+    ****************************************************************
+    TEST DETAILS:
+    - Time to complete test: ${data.state.testRunDurationMs} ms
+    - Environment target: ${__ENV.TARGET_ENV}
+    - Scenario test type: ${__ENV.SCENARIO_TYPE_ENV}
+    - Number of VUs: ${__ENV.VIRTUAL_USERS_ENV} (OK: ${data.metrics.checks.values.passes} - ERRORS: ${data.metrics.checks.values.fails})
+    ****************************************************************`)
     return {
             'stdout': textSummary(data, { indent: ' ', enableColors: true}),
             './performancetest-result.xml': jUnit(data),
