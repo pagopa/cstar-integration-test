1481a9179336cd758b9f46a1163934b72f85123d
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2023-03-14 10:33:19 +0100

    IDP-445 deleted unnecessary logs

--- test/performance/rtdTransactionsFile.js
+++ test/performance/rtdTransactionsFile.js
@@ -114,13 +114,6 @@ export default () => {
 
 export function handleSummary(data){
     console.log(`TEST DETAILS: [Time to complete test: ${data.state.testRunDurationMs} ms, Environment target: ${__ENV.TARGET_ENV}, Scenario test type: ${__ENV.SCENARIO_TYPE_ENV}, Number of VUs: ${__ENV.VIRTUAL_USERS_ENV}, Request processed: ${data.metrics.http_reqs.values.count}, Request OK: ${data.metrics.http_req_failed.values.fails}, ERRORS: ${data.metrics.http_req_failed.values.passes}]`)
-    if(__ENV.SCENARIO_TYPE_ENV == 'rampingArrivalRate'){
-        let stringRamping = 'Ramping iterations for stage : { '
-        for(let i=0; i<customStages.length-1; i++){
-            stringRamping += `${customStages[i].target}, `
-        }
-        console.log(stringRamping+ `${customStages[customStages.length-1].target} } `)
-    }
     return {
             'stdout': textSummary(data, { indent: ' ', enableColors: true}),
             './performancetest-result.xml': jUnit(data),
