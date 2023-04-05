f6524bf0ad5a404bbf8c5e43a0e7fc0985b4d56e
larissaaiala <larissaaialasilva@gmail.com>
2023-04-05 09:37:15 +0200

    [IDP-445] maxVus

--- test/common/stageUtils.js
+++ test/common/stageUtils.js
@@ -1,39 +1,63 @@
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
-export function setStages(tempVus, stageNumber){
+export function setStages(tempVus, stageNumber) {
     const arr = new Array(stageNumber);
-    for (let i = stageNumber-1; i >= 0; i--) {
-        if(i == stageNumber-1){
-            arr[i] = {duration: '1s', target: 0}
-        } else if(i == 0) {
-           arr[i] = {duration: '1s', target: tempVus}
-           tempVus -= tempVus
+    for (let i = stageNumber - 1; i >= 0; i--) {
+        if (i == stageNumber - 1) {
+            arr[i] = { duration: '1s', target: 0 }
+        } else if (i == 0) {
+            arr[i] = { duration: '1s', target: tempVus }
+            tempVus -= tempVus
         } else {
-            let r = randomIntBetween(1, (tempVus/2)-1)
-            arr[i] = {duration: '1s', target: r}
+            let r = randomIntBetween(1, (tempVus / 2) - 1)
+            arr[i] = { duration: '1s', target: r }
             tempVus -= r
         }
     }
     return arr;
 }
-export function setScenarios(vus, maxVus, startTime, maxDuration){
-    let scenarios = {}
-    let counter = 0
-    do {
-        let randomVus = randomIntBetween(1, maxVus)
-        let actualVus = vus <= randomVus ? vus : randomVus
-        const propertyName = `scenario_${counter}`
-        scenarios[propertyName]= {
-            executor: 'per-vu-iterations',
-            vus: actualVus,
-            iterations: 1,
-            startTime: `${startTime}s`,
-            maxDuration: `${maxDuration}s`
+export function setScenarios(vus, maxVus, startTime, maxDuration) {
+    if (maxVus == 0) {
+        let scenarios = {}
+        let counter = 0
+        do {
+            let randomVus = randomIntBetween(1, vus/2)
+            let actualVus = vus <= randomVus ? vus : randomVus
+            const propertyName = `scenario_${counter}`
+            scenarios[propertyName] = {
+                executor: 'per-vu-iterations',
+                vus: actualVus,
+                iterations: 1,
+                startTime: `${startTime}s`,
+                maxDuration: `${maxDuration}s`
+            }
+            startTime = parseInt(startTime) + parseInt(maxDuration)
+            counter++
+            vus -= randomVus
         }
-        startTime=parseInt(startTime)+parseInt(maxDuration)
-        counter++
-        vus -= randomVus
+        while (vus > 0)
+        console.log('scenarios', scenarios)
+        return scenarios
     }
-    while(vus > 0)
-    return scenarios
+    if (maxVus > 0) {
+        let scenarios = {}
+        let counter = 0
+        do {
+            let randomVus = randomIntBetween(1, maxVus)
+            let actualVus = vus <= randomVus ? vus : randomVus
+            const propertyName = `scenario_${counter}`
+            scenarios[propertyName] = {
+                executor: 'per-vu-iterations',
+                vus: actualVus,
+                iterations: 1,
+                startTime: `${startTime}s`,
+                maxDuration: `${maxDuration}s`
+            }
+            startTime = parseInt(startTime) + parseInt(maxDuration)
+            counter++
+            vus -= randomVus
+        }
+        while (vus > 0)
+        return scenarios
+    }
 }
