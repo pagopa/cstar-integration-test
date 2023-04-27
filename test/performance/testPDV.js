7525cdbc509b5a3c850bee054008b3bff64132f3
larissaaiala <larissaaialasilva@gmail.com>
2023-04-27 16:46:53 +0200

    IDP-782 test pdv

---
+++ test/performance/testPDV.js
@@ -0,0 +1,77 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { Counter } from 'k6/metrics';
+
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      executor: 'constant-arrival-rate',
+
+      // test duration
+      duration: '5m',
+
+      // test rate
+      rate: 300,
+
+      // It should start `rate` iterations per second
+      timeUnit: '1s',
+
+      // pre-allocate vu
+      preAllocatedVUs: 300,
+
+      // max allowed vu
+      maxVUs: 600,
+    },
+  },
+};
+
+const apiVersion = 'v1';
+const throttling = new Counter('throttling');
+const services = JSON.parse(open('../../services/environments.json'))
+const random = (length = 8) => {
+    // Declare all characters
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    // Pick characters randomly
+    let str = '';
+    for (let i = 0; i < length; i++) {
+        str += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return str;
+
+};
+
+export default function () {
+
+    var apiKey = `${__ENV.APIM_SK}`
+
+  baseUrl = services[`${__ENV.TARGET_ENV}_pdv`].baseUrl
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': apiKey
+    },
+  };
+
+  var payload = JSON.stringify(
+    {
+        'pii': random(4)
+    }
+);
+
+  var r = http.put(url, payload, params);
+
+  console.log(`Status ${r.status}`);
+
+  check(r, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  if (r.status === 429) {
+    throttling.add(1);
+  }
+
+}
