6578f1add3f893612607ddf4a357edf8dd1c53f2
larissaaiala <larissaaialasilva@gmail.com>
2023-06-21 10:50:38 +0200

    IDP-886-test-performance-onboarding-workflow test only T&C (isolate getInitiative) rollback

--- test/performance/idpayOnboardingAPI.js
+++ test/performance/idpayOnboardingAPI.js
@@ -133,7 +133,7 @@ export default () => {
         exec.test.abort()
     }
 
-    /* if (checked) {
+    if (checked) {
         const serviceId = `${__ENV.SERVICE_ID}`
         const params = {
             headers: {
@@ -158,7 +158,7 @@ export default () => {
 
         const bodyObj = JSON.parse(res.body)
         init = bodyObj.initiativeId
-    } */
+    }
 
     group('Should onboard Citizen', () => {
 
@@ -166,7 +166,7 @@ export default () => {
             if (checked) {
 
                 const body = {
-                    initiativeId: `${__ENV.INITIATIVE_ID}` // -e SERVICE_ID=${{ parameters.SERVICE_ID }} \
+                    initiativeId: init
                 }
                 let res = putOnboardingCitizen(
                     baseUrl,
@@ -182,7 +182,6 @@ export default () => {
             }
 
          })
-         
         /*group('Check accepted status', () => {
             if (checked) {
                 const params = init
