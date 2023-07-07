dd0129e6282997b1daa4cd6278e4a4a31cdddd86
anttorre <antonio.torre@nttdata.com>
2023-07-07 17:17:17 +0200

    fix mil path

--- test/common/api/idpay/idPayPaymentDiscount.js
+++ test/common/api/idpay/idPayPaymentDiscount.js
@@ -1,8 +1,9 @@
 import http from 'k6/http'
 
 const API_PREFIX = '/idpay/payment/qr-code'
+const API_MIL_PREFIX = '/idpay/mil/payment/qr-code/merchant'
 export function createTransaction(baseUrl, body, params) {
-    const res = http.post(`${baseUrl}${API_PREFIX}/merchant`, body, params)
+    const res = http.post(`${baseUrl}${API_MIL_PREFIX}`, body, params)
 
     __ENV.REQ_DUMP === undefined || console.log(JSON.stringify(res, null, 2))
     return res
