0d3ac41a8cc5a4214097c103422592e8f1abd6ef
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2023-08-23 17:30:00 +0200

    updated idpayInitiative

--- test/common/api/idpay/idpayInitiative.js
+++ test/common/api/idpay/idpayInitiative.js
@@ -9,8 +9,8 @@ export const INITIATIVE_API_NAMES = {
 // Environments allowed to be tested
 const REGISTERED_ENVS = [DEV, UAT]
 
-const innerBaseUrl = `${getBaseUrl(REGISTERED_ENVS, 'internal')}/idpayinitiative`
-const apimBaseUrl = getBaseUrl(REGISTERED_ENVS, 'io') // api-io services baseUrl
+const innerBaseUrl = `${getBaseUrl(REGISTERED_ENVS, 'internal')}/idpayportalwelfarebackendinitiative`
+// const apimBaseUrl = getBaseUrl(REGISTERED_ENVS, 'io') // api-io services baseUrl
 
 const API_PREFIX = '/idpay/initiative'
 
@@ -18,7 +18,7 @@ const API_PREFIX = '/idpay/initiative'
 export function deleteInitiative(){
     const apiName = INITIATIVE_API_NAMES.deleteInitiative
 
-    let url = `${apimBaseUrl}${API_PREFIX}/${INITIATIVE_ID}`
+    let url = `${innerBaseUrl}${API_PREFIX}/${INITIATIVE_ID}`
 
     const res = http.delete(url)
     logResult(apiName, res)
