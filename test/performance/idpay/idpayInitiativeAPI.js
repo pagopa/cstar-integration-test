2edacd0b2d5e5d817bef9901b5d3dc471760ea02
Giuseppe-LaManna <127106061+Giuseppe-LaManna@users.noreply.github.com>
2023-08-31 10:06:03 +0200

    feat: Idp 1600 add stage delete su pipeline di performance (#106)
    * initialized script for delete initiative
    
    * update idpayInitiativeAPI
    
    * updated import
    
    * updated import
    
    * updated idpayInitiative
    
    * updated idpayInitiative
    
    * fixed initiativeid
    
    * added onboarding and payment steps
    
    * added onboarding and payment steps
    
    * IDP-1600 updated http method
    
    * IDP-1600-initialized script for delete initiative
    
    * update idpayInitiativeAPI
    
    * updated import
    
    * updated import
    
    * updated idpayInitiative
    
    * updated idpayInitiative
    
    * fixed initiativeid
    
    * added onboarding and payment steps
    
    * added onboarding and payment steps
    
    * IDP-1600 updated http method
    
    * IDP-1600 modified deploy-pipelines
    
    * IDP-1600 changed scenarios
    
    * IDP-1600 modified maxDuration
    
    * IDP-1600 modified maxDuration
    
    * IDP-1600 excluded summary configuration
    
    * IDP-1600 Added script on deploy-pipelines
    
    * IDP-1600 added script on deploy-pipelines
    
    * IDP-1600 fix script identation
    
    * IDP-1600 added pool
    
    * IDP-1600 fix script
    
    * IDP-1600 update deploy-pipelines
    
    * IDP-1600 built a VuIteration scenarios
    
    * IDP-1600 fix declaration
    
    * IDP-1600 fix function name
    
    * IDP-1600 update import
    
    * IDP-1600 update scenario
    
    * IDP-1600 update deploy-pipelines
    
    * IDP-1600 update idPayInitiativeAPI
    
    ---------
    
    Co-authored-by: larissaaiala <larissaaiala.silvaleite.st@nttdata.com>

---
+++ test/performance/idpay/idpayInitiativeAPI.js
@@ -0,0 +1,42 @@
+import { group } from 'k6'
+import {
+    deleteInitiative,
+    INITIATIVE_API_NAMES
+} from '../../common/api/idpay/idpayInitiative.js'
+import { getFCList, getUserIdsList } from '../../common/utils.js'
+import { SharedArray } from 'k6/data'
+import defaultHandleSummaryBuilder from '../../common/handleSummaryBuilder.js'
+import { CONFIG } from '../../common/dynamicScenarios/envVars.js'
+import {
+    assert,
+    statusNoContent,
+} from '../../common/assertions.js'
+import { logErrorResult } from '../../common/dynamicScenarios/utils.js'
+import {
+    IDPAY_CONFIG
+} from '../../common/idpay/envVars.js'
+
+// test tags
+const application = 'idpay'
+const testName = 'idpayInitiativeAPI'
+
+// K6 summary configuration
+export const handleSummary = defaultHandleSummaryBuilder(application, testName)
+
+export default () => {
+
+    group('Delete initiative', () => {
+        
+        const res = deleteInitiative(
+            CONFIG.USE_INTERNAL_ACCESS_ENV,
+            IDPAY_CONFIG.CONTEXT_DATA.initiativeId
+        )
+
+        assert(res, [statusNoContent()])
+        if (res.status != 204) {
+            logErrorResult('deleteInitiative', res, true)
+            return
+        }
+        
+    })
+}
