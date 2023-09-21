161a4fd0726a53d0e220d005376a9bd9d0acac5c
petretiandrea <petretiandrea@gmail.com>
2023-09-21 15:08:25 +0200

    trying add state to verify wallet creation

---
+++ test/performance/idpay/idpayVerifyOnboarding.js
@@ -0,0 +1,66 @@
+import { group, sleep, check } from 'k6'
+import {
+    IDPAY_CONFIG,
+    getIdPayScenarioUserToken,
+} from '../../common/idpay/envVars.js'
+import { getFCList, getUserIdsList } from '../../common/utils.js'
+import { CONFIG } from '../../common/dynamicScenarios/envVars.js'
+import {defaultApiOptionsBuilder} from "../../common/dynamicScenarios/defaultOptions";
+import {ONBOARDING_API_NAMES} from "../../common/api/idpay/idpayOnboardingCitizen";
+import {getWalletDetail, WALLET_API_NAMES} from "../../common/api/idpay/idpayWallet";
+import {logErrorResult} from "../../common/dynamicScenarios/utils";
+
+// Set up data for processing, share data among VUs
+const usersList = new SharedArray(
+    'usersList',
+    CONFIG.USE_INTERNAL_ACCESS_ENV ? getUserIdsList : getFCList
+)
+
+// test tags
+const application = 'idpay'
+const testName = 'idpayVerifyOnboarding'
+
+console.log("Waiting 1 minute before verify")
+sleep(60) // 1 minute
+console.log("Starting verify")
+
+export const options = defaultApiOptionsBuilder(
+    application,
+    testName,
+    Object.values(ONBOARDING_API_NAMES)
+        .filter(
+            (api) =>
+                api !== ONBOARDING_API_NAMES.checkInitiativeBudget &&
+                api !== ONBOARDING_API_NAMES.getInitiative
+        )
+        .concat({
+            apiName: WALLET_API_NAMES.getWalletDetail,
+            maxHttpReqFailedRate: 0.999,
+        }) // applying apiName tags to thresholds
+)
+
+export default () => {
+    // selecting current scenario/iteration test token
+    const token = getIdPayScenarioUserToken(usersList)
+
+    group('Wallet must exist', () => {
+        const res = getWalletDetail(
+            CONFIG.USE_INTERNAL_ACCESS_ENV,
+            token,
+            IDPAY_CONFIG.CONTEXT_DATA.initiativeId
+        )
+
+        check(res, {
+            'HTTP status is 200 or 404': (r) =>
+                r.status === 200 || r.status === 404,
+        })
+
+        if (res.status === 404) {
+            logErrorResult(
+                `Wallet associated to user with token [${token}] not found`,
+                res,
+                true
+            )
+        }
+    })
+};
